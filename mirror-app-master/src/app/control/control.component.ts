0ad5cd32ce42bad39940b48be17ea1462a995cb5
tasnia.heya <tasnia.heya@gmail.com>
2019-11-03 18:34:08 -0600

    control, menu and schedule

--- mirror-app-master/src/app/control/control.component.ts
+++ mirror-app-master/src/app/control/control.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as p5 from 'p5';
 
 @Component({
   selector: 'app-control',
@@ -9,7 +10,56 @@ export class ControlComponent implements OnInit {
 
   constructor() { }
 
+
   ngOnInit() {
+    const sketch = (s) => {
+      let img_black_tint;
+    
+      let image_round;
+      let images = [];
+  
+
+      s.preload = () => {
+        images[0] = s.loadImage('assets/sun.png');
+        images[1] = s.loadImage('assets/settings.png');
+        images[2] = s.loadImage('assets/rotation_lock.png');
+        images[3] = s.loadImage('assets/camera.png');
+        img_black_tint = s.loadImage('assets/black_tint.png');
+        image_round = s.loadImage('assets/round.png');  
+    }
+
+      s.setup = () => {
+        var myCanvas = s.createCanvas(480, 70);
+        myCanvas.parent("control");
+       
+
+      };
+
+      s.draw = () => {
+       
+        s.strokeWeight(0);
+        let c = s.color(120,120,120,100);
+        s.fill(c);    
+        s.image(image_round, 10,5 ,60,60);
+        s.textSize(14);
+        s.fill(255, 255, 255);
+
+        s.image(img_black_tint, 80, 5, 60, 60);
+        s.image(images[0], 93, 18, 35, 35);
+
+        s.image(img_black_tint, 2*80, 5, 60, 60);
+        s.image(images[1], 173, 18, 35, 35);
+
+        s.image(img_black_tint, 3*80, 5, 60, 60);
+        s.image(images[2], 253, 18, 35, 35);
+
+        s.image(img_black_tint, 4*80, 5, 60, 60);
+      };
+
+    }
+
+    let canvas = new p5(sketch);
   }
+
 
 }
--- mirror-app-master/src/app/schedule/schedule.component.ts
+++ mirror-app-master/src/app/schedule/schedule.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import * as p5 from 'p5';
+ 
+
 
 @Component({
   selector: 'app-schedule',
@@ -7,9 +10,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleComponent implements OnInit {
 
+  
+  time: string;
+  event: string;
+ 
+
+
   constructor() { }
 
   ngOnInit() {
+    const sketch = (s) => {
+      let red;
+      let blue;
+      let back
+      
+      let height = 150;
+      let width = 400;
+      
+      let base_x = 10;
+      let base_y =10;
+      
+      let image_gap =5;
+
+
+      s.preload = () => {
+        red = s.loadImage('assets/red.png');
+        blue = s.loadImage('assets/blue.png');
+        back = s.loadImage('assets/back.png');
+    }
+
+      s.setup = () => {
+        var myCanvas = s.createCanvas(1440, 50);
+        myCanvas.parent("schedule");
+
+      };
+
+      s.draw = () => {
+        //s.background(220);
+        //s.image(back, base_x, base_y, width + image_gap, height + (image_gap*4));
+  
+        s.textSize(20);
+        s.fill(255, 255, 255);
+  
+
+       
+          s.image(red, 400, 0, 400, 0);
+          s.text("Intro to Artificial Intelligence", 540, 30); 
+          s.text("2:00-2:50", 420, 30);
+       
+      
+         s.textSize(20);
+         s.image(blue, 810, 0, 400, 0);  
+         s.text("4:00-10:00", 820, 30);
+      
+         s.text("Work", 950, 30);   
+       
+       
+        
+        
+  
+        // s.image(blue, base_x+ (width/3), base_y + image_gap*2 + height/3, width* (2/3), height/3);
+        // s.text('10:00 - 2:00', base_x+ (width/15), (base_y + height/4 + height/3 + image_gap));
+        // s.text('Work', base_x+ (width/3) + image_gap*3, base_y + height/4 + height/3 );
+  
+  
+        // s.image(red, base_x+ (width/3), base_y + image_gap*3 + (height/3) * 2, width* (2/3), height/3);
+        // s.text('3:00 - 3:50', base_x+ (width/15), ( base_y + height/4 + (height/3)*2) + image_gap) ;
+        // s.text('Human Computer Interaction', base_x+ (width/3) + image_gap*3, (base_y + height/4 + (height/3)*2 + image_gap) );
+        
+        
+      };
+
+
+      s.selfDestruct = () => {
+        s.remove();
+      };
+    }
+
+    let canvas = new p5(sketch);
   }
 
 }
